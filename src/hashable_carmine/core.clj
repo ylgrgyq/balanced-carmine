95588f85d2fdd6d4d81f9ab9aaec5a2673109e3a
Rui Guo <rguo@leancloud.rocks>
2017-07-09 17:20:12 +0800

    (feat) hashable wcar

--- src/hashable_carmine/core.clj
+++ src/hashable_carmine/core.clj
@@ -1,12 +1,14 @@
 (ns hashable-carmine.core
   (:require [taoensso.carmine :as car]
-            [taoensso.carmine.commands :as cmds])
+            [taoensso.carmine.commands :as cmds]
+            [hashable-carmine.hash :as hash])
   (:import (com.google.common.hash Hashing HashCode)))
 
 (defn- get-spec [hash-key hash-fn candidate-specs]
   (println "get spec" hash-key candidate-specs)
   (if (string? hash-key)
-    (hash-fn hash-key candidate-specs)
+    (nth candidate-specs
+         (hash-fn hash-key candidate-specs))
     (first candidate-specs)))
 
 (defn default-hash-fn [hash-key candidate-specs]
@@ -15,50 +17,69 @@
            (HashCode/fromBytes)
            (Hashing/consistentHash (count candidate-specs)))))
 
-(defn parse-hash-key [args]
-  (when-not (list? (first args))
+
+
+(defn- parse-hash-key [args]
+  (when-not (or (= :as-pipeline (first args))
+           (list? (first args)))
     (first args)))
 
 (defmacro wcar
-  {:arglists '([conn-opts :as-pipeline & body] [conn-opts & body])}
+  "It's the same as taoensso.carmine/wcar, but supports
+      :specs-group [{:host \"127.0.0.1\" :port 6379}
+                    {:host \"127.0.0.1\" :port 6380}
+                    {:host \"127.0.0.1\" :port 6381}]
+   in conn for redis load balance.
+  "
+  {:arglists '([conn-opts hash-key :as-pipeline & body]
+               [conn-opts hash-key & body]
+               [conn-opts :as-pipeline & body]
+               [conn-opts & body])}
   [conn-opts & args]                                        ; [conn-opts & [a1 & an :as args]]
-  `(let [{specs-group# :specs-group hash-fn# :hash-fn
-          :or          {hash-fn# default-hash-fn}} ~conn-opts
-         spec#      (get-spec ~(when-not (list? (first args))
-                                 (first args))
-                              default-hash-fn
-                              specs-group#)
-         conn-opts# (assoc ~conn-opts :spec spec#)]
-     (println "conn opts" conn-opts#)
-     (car/wcar conn-opts#
-       ~@args)))
-
-#_(defmacro wcar*
-  [& body]
-  `(let [spec# (get-spec ~(when-not (list? (first body))
-                            (first body)))]
-     (try
-       (diehard/with-circuit-breaker (:breaker spec#)
-                                     (car/wcar {:pool pool :spec spec#}
-                                               ~@body))
-       (catch CircuitBreakerOpenException _#
-         (log/warn "Offline redis disabled with circuit breaker" (select-keys spec# [:host :port])))
-       (catch Exception e#
-         (log/warn e# "Failed to call offline redis" (select-keys spec# [:host :port]))))))
+  (let [hash-key (gensym 'hash-key)]
+    `(let [{specs-group# :specs-group hash-fn# :hash-fn
+            :or          {hash-fn# default-hash-fn}} ~conn-opts
+           ~hash-key  ~(parse-hash-key args)
+           spec#      (get-spec ~hash-key
+                                hash-fn#
+                                specs-group#)
+           conn-opts# (assoc ~conn-opts :spec spec#)]
+       (car/wcar conn-opts#
+                 ~(if hash-key
+                    `(do ~@(rest args))
+                    `(do ~@args))))))
 
 (comment
-  (let [pool {:max-total-per-key (* 3 (.availableProcessors (Runtime/getRuntime)))
-              :min-idle-per-key (* 3 (.availableProcessors (Runtime/getRuntime)))
-              :max-wait-ms 5000}
-        specs [{:host "127.0.0.1" :port 6379}
-               {:host "127.0.0.1" :port 6380}
-               {:host "127.0.0.1" :port 6381}]
+  (let [pool         {:max-total-per-key (* 3 (.availableProcessors (Runtime/getRuntime)))
+                      :min-idle-per-key  (* 3 (.availableProcessors (Runtime/getRuntime)))
+                      :max-wait-ms       5000}
+        specs        [{:host "127.0.0.1" :port 6379}
+                      {:host "127.0.0.1" :port 6380}
+                      {:host "127.0.0.1" :port 6381}]
         server1-conn {:pool pool :spec {} :specs-group specs}]
     (println
       (wcar server1-conn "sss"
             (car/set "a" 100)
             (car/get "a")
-            #_(car/del "a")
-            ))))
+            (car/del "a")))))
+
+(comment
+  (def pool {:max-total-per-key (* 3 (.availableProcessors (Runtime/getRuntime)))
+             :min-idle-per-key  (* 3 (.availableProcessors (Runtime/getRuntime)))
+             :max-wait-ms       5000})
+  (def specs [{:host "127.0.0.1" :port 6379}
+              {:host "127.0.0.1" :port 6380}
+              {:host "127.0.0.1" :port 6381}])
+  (defmacro wcar*
+    [& body]
+    `(wcar {:pool pool
+            :specs-group specs}
+           ~@body))
+  (println
+    (wcar* "hello"
+          (car/set "a" 100)
+          (car/get "a"))))
+
+
 
 
--- src/hashable_carmine/hash.clj
+++ src/hashable_carmine/hash.clj
@@ -1,1 +1,27 @@
 (ns hashable-carmine.hash)
+
+(defn linear-congruential-next-double [^long state]
+  (println state (type state) (unchecked-multiply state (long 2862933555777941757)))
+  (let [state (+ (unchecked-multiply state (long 2862933555777941757)) 1)]
+    (* )
+    [state
+     (/ (double (+ (int (unsigned-bit-shift-right state 33)) 1))
+        2.147483648E9)]))
+
+(defn consistent-hash [input buckets-count]
+  (loop [state input candidate 0]
+    (let [[state next-double] (linear-congruential-next-double state)
+          next (int (/ (double (+ candidate 1)) next-double))]
+      (if (or (neg? next) (>= next buckets-count))
+        candidate
+        (recur state next)))))
+
+(defn hash-code [^String key]
+  (let [bs (.getBytes key)
+        ret (long (bit-and (first bs) 0xFF))
+        index-limit (min (count bs) 8)]
+    (loop [ret ret index 1]
+      (if (< index index-limit)
+        (let [b (bit-shift-left (bit-and (long (get bs index)) 0xFF) (* index 8))]
+          (recur (bit-or ret b) (inc index)))
+        ret))))
